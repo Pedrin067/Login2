f1d8d2f72a2c186e31125fb253819ecf0ba3aba9
Aluno IFMT-PDL <aluno@ifmt.edu>
2023-11-08 18:13:48 -0400

    inicializando o projeto login2

---
+++ src/main/java/com/pedrohenrique/login2/TelaLoginController.java
@@ -0,0 +1,33 @@
+package com.pedrohenrique.login2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class TelaLoginController {
+
+    @FXML
+    private Hyperlink cadastrar;
+
+    @FXML
+    private Label nomeLabel;
+
+    @FXML
+    private TextField nomeText;
+
+    @FXML
+    private Label senhaLabel;
+
+    @FXML
+    private PasswordField senhaText;
+
+    @FXML
+    void cadastro(ActionEvent event) {
+
+    }
+
+}
+
